e5b6c53ca3b9dc3cd938078d99ffca1684711c03
kouta718 <qooron1927@gmail.com>
2025-02-14 23:43:53 +0900

    2/14/25

---
+++ counter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(() => {
+    const $counter = document.getElementById("js-counter");
+
+    const ClickHandler = (e) => {
+        const $targetButton = e.currentTarget;
+        let currentCount = parseInt($counter.textContent);
+        if($targetButton.textContent === "+"){
+            $counter.textContent = currentCount + 1;
+        }else{
+            $counter.textContent = currentCount - 1;
+        }
+    }
+
+    for (let index = 0; index < document.getElementsByClassName("js-button"),length; index++) {
+    document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => ClickHandler(e))
+    }
+})();
