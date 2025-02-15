8f7a68c282673ab671cee7f8be39c93f317a5f87
kouta718 <qooron1927@gmail.com>
2025-02-15 23:07:40 +0900

    2/15/25-11:07pm

--- counter.js
+++ counter.js
@@ -1,8 +1,9 @@
 'use strict';
 
-(() => {
-    const $counter = document.getElementById("js-counter");
+const $counter = document.getElementById("js-counter");
 
+// カウンター基本操作
+(() => {
     const ClickHandler = (e) => {
         const $targetButton = e.currentTarget;
         let currentCount = parseInt($counter.textContent);
@@ -19,5 +20,29 @@
 
     for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
         document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => ClickHandler(e))
+    }
+})();
+
+// 任意の値でカウントする操作
+(() => {
+    const ClickHandler = (e) => {
+        const $anyNumber = parseFloat(document.getElementById("js-any-number").value);
+        if(isNaN($anyNumber)) {
+            errorMsg.textContent = "エラー: 必要な入力値を正しく入力してください。";
+            return;
+        }
+
+        document.getElementById("errorMsg").textContent = "";
+        const $targetButton = e.currentTarget;
+        let currentCount = parseInt($counter.textContent);
+        if($targetButton.textContent === "+"){
+            $counter.textContent = currentCount + $anyNumber;
+        } else {
+            $counter.textContent = currentCount - $anyNumber;
+        }
+    }
+
+    for (let index = 0; index < document.getElementsByClassName("js-any-button").length; index++) {
+        document.getElementsByClassName("js-any-button")[index].addEventListener("click", (e) => ClickHandler(e))
     }
 })();
--- reset.js
+++ reset.js
@@ -1,10 +1,14 @@
 'use strict';
 
+// リセット操作
 (() => {
     const $counter = document.getElementById("js-counter");
 
     document.getElementById("js-reset-button").addEventListener("click", () => {
-        $counter.textContent = 0;
-        document.getElementById("js-any-number").value = "";
+        let check = window.confirm('リセットしますか？');
+        if(check === true){
+            $counter.textContent = 0;
+            document.getElementById("js-any-number").value = "";
+        }
     });
 })();
